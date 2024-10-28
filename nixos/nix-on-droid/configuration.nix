01a6743205c451ce076bb82e41392c1de23e3d72
Gokul Swaminathan <gokulswamilive@gmail.com>
2024-10-28 02:50:15 -0700

    feat: add comma

--- nixos/nix-on-droid/configuration.nix
+++ nixos/nix-on-droid/configuration.nix
@@ -2,7 +2,6 @@
   config,
   lib,
   pkgs,
-  inputs,
   ...
 }: {
   # Backup etc files instead of failing to activate generation if a file already exists in /etc
@@ -27,41 +26,11 @@
     home.packages = lib.attrValues {
       inherit
         (pkgs)
+        comma
         gh
         git
         neovim
         ;
-    };
-  };
-
-  nix = {
-    # This will add each flake input as a registry
-    # To make nix commands consistent with your flake
-    registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
-
-    # This will additionally add your inputs to the system's legacy channels
-    # Making legacy nix commands consistent as well, awesome!
-    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
-
-    settings = {
-      # Enable flakes and new 'nix' command
-      experimental-features = "nix-command flakes";
-      # Deduplicate and optimize nix store
-      auto-optimise-store = true;
-
-      substituters = [
-        "https://cache.nixos.org?priority=10"
-        "https://cache.ngi0.nixos.org/"
-        "https://nix-community.cachix.org"
-        "https://fortuneteller2k.cachix.org"
-      ];
-
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "fortuneteller2k.cachix.org-1:kXXNkMV5yheEQwT0I4XYh1MaCSz+qg72k8XAi2PthJI="
-      ];
     };
   };
 
