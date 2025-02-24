27f510da29e3b4bbcaf08e7cd1ba0560e6985e8d
Gokul Swaminathan <gokulswamilive@gmail.com>
2025-02-23 17:32:08 -0800

    fix droid config

--- nixos/nix-on-droid/configuration.nix
+++ nixos/nix-on-droid/configuration.nix
@@ -6,27 +6,7 @@
   pkgs,
   ...
 }: {
-  nix = {
-    package = pkgs.lix;
-
-    settings = {
-      experimental-features = "nix-command flakes";
-
-      substituters = [
-        "https://cache.nixos.org"
-        "https://cache.ngi0.nixos.org"
-        "https://nix-community.cachix.org"
-        "https://nix-on-droid.cachix.org"
-      ];
-
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "nix-on-droid.cachix.org-1:56snoMJTXmDRC1Ei24CmKoUqvHJ9XCp+nidK7qkMQrU="
-      ];
-    };
-  };
+  nix.package = pkgs.lix;
 
   nixpkgs = {
     config.allowUnfree = true;
